838fd15b489c4427f6c041d193c77036930f2208
Zachary <zacarlin@gmail.com>
2018-12-04 22:24:43 -0500

    nuxt snowman

---
+++ components/lights.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+       <li></li>
+        <li></li>
+         <li></li>
+          <li></li>
+           <li></li>
+            <li></li>
+             <li></li>
+              <li></li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'lights'
+  }
+
+</script>
+<style scoped>
+  ul {
+    position: absolute;
+    width: 100%;
+    margin: 20px 0 0;
+    padding: 0;
+    border-top: 2px solid #777;
+    display: flex;
+
+  }
+
+  ul li {
+    list-style: none;
+    position: relative;
+    top: -20px;
+    width: 24px;
+    height: 40px;
+    border-radius: 50%;
+    background: rgba(255, 255, 255, .1);
+    margin: 25px;
+
+  }
+
+  ul li::before {
+    content: '';
+    position: absolute;
+    top: -7px;
+    left: 7px;
+    width: 10px;
+    height: 8px;
+    background: #444;
+  }
+
+  ul li:after {
+    content: '';
+    position: absolute;
+    top: -24px;
+    width: 74px;
+    height: 26px;
+    border-bottom: 2px solid #444;
+    border-radius: 50%;
+  }
+
+  ul li:last-child:after {
+    border-bottom: none;
+  }
+
+  ul li:nth-child(3n+1) {
+    background: rgb(255, 255, 0, 1);
+    animation: animate1 2.5s linear infinite;
+  }
+
+  @keyframes animate1 {
+    0% {
+      background-color: rgb(255, 255, 0, .2);
+    }
+
+    50% {
+      background-color: rgb(255, 255, 0, 1);
+      box-shadow: 0 0 25px rgb(255, 255, 0, 1), 0 0 45px rgb(255, 255, 0, 1)
+    }
+
+    100% {
+      background-color: rgb(255, 255, 0, .2);
+    }
+  }
+
+    ul li:nth-child(3n+2) {
+      background-color: rgb(255, 0, 255, 1);
+    animation: animate2 2.5s linear infinite;
+    animation-delay: .9s;
+  }
+
+  @keyframes animate2 {
+    0% {
+      background-color: rgb(255, 0, 255, .2);
+    }
+
+    50% {
+      background-color: rgb(255, 0, 255, 1);
+      box-shadow: 0 0 25px rgb(255, 0, 255, 1), 0 0 45px rgb(255, 0,255 , 1)
+    }
+
+    100% {
+      background-color: rgb(255, 0, 255, .2);
+    }
+  }
+
+    ul li:nth-child(3n+3) {
+      background-color: rgb(0, 162, 255,1);
+    animation: animate3 2.5s linear infinite;
+    animation-delay: 1.9s;
+  }
+
+  @keyframes animate3 {
+    0% {
+      background-color: rgb(0, 162, 255,.2);
+    }
+
+    50% {
+      background-color: rgb(0, 162, 255,1);
+      box-shadow: 0 0 25px rgb(0, 162, 255,1), 0 0 45px rgb(0, 162, 255,1)
+    }
+
+    100% {
+      background-color: rgb(0, 162, 255,.2);
+    }
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <snow></snow>
+    <lights></lights>
+    <br><br><br><br>
+    <span>
+      <h2>Chistams is in</h2>
+      <Countdown deadline="December 25, 2018"></Countdown>
+    </span>
+    <snowman></snowman>
+  </div>
+</template>
+
+<script>
+  import snowman from '~/components/snowman.vue'
+  import lights from '~/components/lights.vue'
+  import Countdown from 'vuejs-countdown'
+  import snow from '~/components/snow.vue'
+
+  export default {
+    components: {
+      Countdown,
+      snowman,
+      lights,
+      snow
+    }
+
+  }
+
+</script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  span {
+    display: inline-block;
+    text-align: center;
+    width: 100vw;
+    color: white;
+  }
+
+  span h2 {
+    padding-right: 10px;
+    
+  }
+</style>
